486a914883d97bc99eb14a487d193c1d419354e3
ihorshein <ihorshein@htmail.com>
2024-10-20 20:05:08 +0200

    HW for Lesson 4 code style fixes

--- src/main/java/org/ihorshein/lesson4/model/UserResponseDto.java
+++ src/main/java/org/ihorshein/lesson4/model/UserResponseDto.java
@@ -3,6 +3,6 @@ package org.ihorshein.lesson4.model;
 public record UserResponseDto(Long userId, String email, String phoneNumber) {
   @Override
   public String toString() {
-    return "userId=" + userId + ", email='" + email + "', phoneNumber='" + phoneNumber + "'";
+    return "userId = " + userId + ", email = '" + email + "', phoneNumber = '" + phoneNumber + "'";
   }
 }
